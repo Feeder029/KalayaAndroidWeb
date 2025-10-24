c68416fa383e321566d7ba806c9b52a0af6161c0
Feeder1129 <mrmm112903@gmail.com>
2025-10-25 00:43:43 +0800

    home init

--- public/index.php
+++ public/index.php
@@ -4,12 +4,60 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KalayaAndroid</title>
+    <link rel="stylesheet" href="../public/css/index.css">
+    <link href='https://cdn.boxicons.com/fonts/basic/boxicons.min.css' rel='stylesheet'>
+    <link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+  integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+  crossorigin="anonymous"
+  referrerpolicy="no-referrer"
+/>
+
 </head>
 <body>
-    <!-- header -->
-    <?php include '../src/includes/header.php'; ?>
+    <header>
+        <!-- header -->
+        <?php include '../src/includes/header.php'; ?>
+    </header>
+   
 
-    <section style="height: 1200px;"></section>
-    <section></section>
+    <section id="home">
+        <div class="home-container">
+            <div class="title">
+                <h1>KalayaAndroid</h1>
+            </div>
+            <div class="home-download">
+                <button class="download-btn-play">
+                    <i class="fa-solid fa-play"></i>
+                    Play
+                </button>
+                <button class="download-btn-type">
+                    >
+                </button>
+            </div>
+            <div class="info">
+                <h3>A 3D Role-Playing Game About Philippine Revolution for Android</h3>
+            </div>
+        </div>
+    </section>
+
+    <section id="features">
+        features
+    </section>
+
+    <section id="about">
+        about
+    </section>
+
+    <section id="system">
+        system
+    </section>
+
+    <section id="contact">
+        contact
+    </section>
+
+    <footer></footer>
 </body>
 </html>
