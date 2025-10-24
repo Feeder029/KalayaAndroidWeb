3ba42fb6f64de90a3ed892e3fc0b247b62b8fff8
Feeder1129 <mrmm112903@gmail.com>
2025-10-24 22:45:44 +0800

    header init

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KalayaAndroid</title>
+</head>
+<body>
+    <!-- header -->
+    <?php include '../src/includes/header.php'; ?>
+
+    <section style="height: 1200px;"></section>
+    <section></section>
+</body>
+</html>
